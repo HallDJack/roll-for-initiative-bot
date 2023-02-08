088a823ac39d482aac89582fbafde7e01239c190
HallDJack <hall.d.jack@gmail.com>
2023-02-08 10:40:17 -0600

    Remove Sterling

--- app/services/initiative.rb
+++ app/services/initiative.rb
@@ -14,7 +14,6 @@ class Initiative
   DEV
   PRODUCT = <<-PRODUCT
 1d20 - Michael
-1d20 - Sterling
 1d20 - Stephanie
 1d20 - Mary
 1d20 - Kim
@@ -24,7 +23,6 @@ class Initiative
 
   EVERYONE = <<-EVERYONE
 1d20 - Michael
-1d20 - Sterling
 1d20 - Stephanie
 1d20 - Jack
 1d20 - Paul
